7601612c756e7a28574ea580be2e7a8a986a21b9
Wicramachine Sergio <wicramachine@gmail.com.com>
2024-07-04 15:49:42 +0200

    Initial Commit

---
+++ Lecteur/LecteurApp.swift
@@ -0,0 +1,17 @@
+//
+//  LecteurApp.swift
+//  Lecteur
+//
+//  Created by Sergio Wicramachine on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LecteurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
